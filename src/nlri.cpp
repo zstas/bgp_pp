19ac4106628a818439a3d3def10da007b2c409bf
Stanislav Zaikin <zstaseg@gmail.com>
2020-11-10 06:31:57 +0000

    nlri part 2

--- src/nlri.cpp
+++ src/nlri.cpp
@@ -21,17 +21,38 @@ NLRI::NLRI( uint8_t *p, uint16_t len ):
 }
 
 NLRI::NLRI( BGP_AFI afi, const std::string &prefix ) {
+    if( auto it = prefix.find( '/' ); it != prefix.npos ) {
+        auto substr = prefix.substr( it + 1 );
+        nlri_len = std::stoi( substr );
+    } else {
+        throw std::runtime_error( "Can't find prefix len" );
+    }
+    auto bytes = nlri_len / 8;
+    if( nlri_len % 8 != 0 ) {
+        bytes++;
+    }
+    data.resize( bytes );
     switch( afi ) {
-    case BGP_AFI::IPv4:
-        data.resize( 4 );
-        if( inet_pton( AF_INET, prefix.c_str(), data.data() ) != 1 ) {
+    case BGP_AFI::IPv4: {
+        if( bytes > 4 ) {
+            throw std::runtime_error( "Prefix len is too long" );
+        }
+        char buf[ 4 ];
+        if( inet_pton( AF_INET, prefix.c_str(), buf ) != 1 ) {
             throw std::runtime_error( "Cannot convert from IPv4 prefix representation" );
         }
-    case BGP_AFI::IPv6:
-        data.resize( 16 );
+        std::copy( buf, buf + bytes, data.data() );
+    }
+    case BGP_AFI::IPv6: {
+        if( bytes > 16 ) {
+            throw std::runtime_error( "Prefix len is too long" );
+        }
+        char buf[ 16 ];
         if( inet_pton( AF_INET6, prefix.c_str(), data.data() ) != 1 ) {
             throw std::runtime_error( "Cannot convert from IPv6 prefix representation" );
         }
+        std::copy( buf, buf + bytes, data.data() );
+    }
     default:
         throw std::runtime_error( "Unknown AF" );
     }
@@ -47,10 +68,29 @@ std::vector<uint8_t> NLRI::serialize() const {
 }
 
 std::string NLRI::as_ipv4() const {
-    // char buf[ INET_ADDRSTRLEN ];
-    // auto ret = inet_ntop( AF_INET, )
+    char buf[ INET_ADDRSTRLEN ];
+    std::array<uint8_t,4> address;
+    std::fill( address.begin(), address.end(), 0 );
+    std::copy( data.begin(), data.end(), address.begin() );
+
+    auto ret = inet_ntop( AF_INET, address.data(), buf, sizeof( buf ) );
+    if( ret != nullptr ) {
+        return ret;
+    } else {
+        return {};
+    }
 }
 
 std::string NLRI::as_ipv6() const {
+    char buf[ INET6_ADDRSTRLEN ];
+    std::array<uint8_t,16> address;
+    std::fill( address.begin(), address.end(), 0 );
+    std::copy( data.begin(), data.end(), address.begin() );
 
+    auto ret = inet_ntop( AF_INET6, address.data(), buf, sizeof( buf ) );
+    if( ret != nullptr ) {
+        return ret;
+    } else {
+        return {};
+    }
 }
